f1fd5e4a1222ad59765a6d4bc64c48b2606013fd
Alice Powell <72016384+AlicePowell@users.noreply.github.com>
2021-03-04 11:26:40 +0000

    Add files via upload

--- mainPage/mainPage.js
+++ mainPage/mainPage.js
@@ -35,16 +35,19 @@ myFunction ();
 var copyright = document.getElementById("creditLink");
 
 function credit(id) {
-    var aP = "Design and photographs by Alice Powell, January 2021. ";
-    var seurat = "Georges Seurat, 'Reclining Man (study for Bathing Place), conte drawing on paper, 1883. ";
-    var leWitt = "Sol Lewitt, 'Drawing Series 1968 (Fours)', graphite wall drawing, 1968. ";
-    var bronze = "Grand Tour bronze, 19th Century. ";
+    var aP = "Coding, design and images by Alice Powell, March 2021. ";
+    // var seurat = "Georges Seurat, 'Reclining Man (study for Bathing Place), conte drawing on paper, 1883. ";
+    var turner = "  //  Joseph Mallord William Turner, 'The Rooftops of Venice, with the Campanile of San Marco and San Giorgio Maggiore at Sunrise', watercolour on paper, 1840.  From the sketchbook, 'Venice: In and around the Hotel Europa', Tate Collection.  "
+    var leWitt = "  //  Sol Lewitt, 'Drawing Series 1968 (Fours)', graphite wall drawing, 1968. ";
+    var bronze = "  //  Grand Tour bronze, 19th Century. ";
+    var plant = "  //  Areca palm, heavily and heavenly influenced by Marcel Broodthaers, 'Un Jardin d'Hiver', 1974.  "
     var mix = aP;
     mix += "\n";
     mix += "\n";
+    mix += turner;
     mix += leWitt;
-    mix += seurat;
     mix += bronze;
+    mix += plant;
     copyright.innerHTML=mix;
     copyright.style.border = "none";
 };
