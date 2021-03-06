27c44a47d7cdabf003f4d827624c2dd455adc296
Alice Powell <72016384+AlicePowell@users.noreply.github.com>
2021-03-06 18:09:28 +0000

    Add files via upload

--- mainPage/mainPage.js
+++ mainPage/mainPage.js
@@ -33,21 +33,28 @@ myFunction ();
 
 // Add credit link
 var copyright = document.getElementById("creditLink");
+var plusline2 = document.getElementById("line2");
+var plusline3 = document.getElementById("line3");
+var plusline4 = document.getElementById("line4");
+var plusline5 = document.getElementById("line5");
+
 
 function credit(id) {
-    var aP = "Coding, design and images by Alice Powell, March 2021. ";
+    var aP = "Coding, design and images by Alice Powell, March 2021.";
     // var seurat = "Georges Seurat, 'Reclining Man (study for Bathing Place), conte drawing on paper, 1883. ";
-    var turner = "  //  Joseph Mallord William Turner, 'The Rooftops of Venice, with the Campanile of San Marco and San Giorgio Maggiore at Sunrise', watercolour on paper, 1840.  From the sketchbook, 'Venice: In and around the Hotel Europa', Tate Collection.  "
-    var leWitt = "  //  Sol Lewitt, 'Drawing Series 1968 (Fours)', graphite wall drawing, 1968. ";
-    var bronze = "  //  Grand Tour bronze, 19th Century. ";
-    var plant = "  //  Areca palm, heavily and heavenly influenced by Marcel Broodthaers, 'Un Jardin d'Hiver', 1974.  "
-    var mix = aP;
-    mix += "\n";
-    mix += "\n";
-    mix += turner;
-    mix += leWitt;
-    mix += bronze;
-    mix += plant;
-    copyright.innerHTML=mix;
+    var turner = "J.M.W. Turner, 'The Rooftops of Venice, with the Campanile of San Marco and San Giorgio Maggiore at Sunrise', watercolour on paper, 1840.  Tate Collection."
+    var leWitt = "Sol Lewitt, 'Drawing Series 1968 (Fours)', graphite wall drawing, 1968.";
+    var bronze = "Grand Tour bronze, 19th Century.";
+    var plant = "Areca palm, heavily and heavenly influenced by Marcel Broodthaers, 'Un Jardin d'Hiver', 1974."
+    copyright.innerHTML=aP;
     copyright.style.border = "none";
+    copyright.style.fontColor = ("green");
+    plusline2.innerHTML=turner;
+    plusline2.style.border = "none";
+    plusline3.innerHTML=leWitt;
+    plusline3.style.border = "none";
+    plusline4.innerHTML=bronze;
+    plusline4.style.border = "none";
+    plusline5.innerHTML=plant;
+    plusline5.style.border = "none";
 };
