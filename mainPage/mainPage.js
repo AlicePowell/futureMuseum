6fba8812536133166d62fa6e1a216e6cadb8b100
Alice Powell <72016384+AlicePowell@users.noreply.github.com>
2021-03-10 17:49:22 +0000

    Add files via upload

--- mainPage/mainPage.js
+++ mainPage/mainPage.js
@@ -37,24 +37,38 @@ var plusline2 = document.getElementById("line2");
 var plusline3 = document.getElementById("line3");
 var plusline4 = document.getElementById("line4");
 var plusline5 = document.getElementById("line5");
+var tally = 0;
 
 
 function credit(id) {
-    var aP = "Coding, design and images by Alice Powell, March 2021.";
+    if (tally == 0) {
+        var aP = "Coding, design and images by Alice Powell, March 2021.";
     // var seurat = "Georges Seurat, 'Reclining Man (study for Bathing Place), conte drawing on paper, 1883. ";
-    var turner = "J.M.W. Turner, 'The Rooftops of Venice, with the Campanile of San Marco and San Giorgio Maggiore at Sunrise', watercolour on paper, 1840.  Tate Collection."
-    var leWitt = "Sol Lewitt, 'Drawing Series 1968 (Fours)', graphite wall drawing, 1968.";
-    var bronze = "Grand Tour bronze, 19th Century.";
-    var plant = "Areca palm, heavily and heavenly influenced by Marcel Broodthaers, 'Un Jardin d'Hiver', 1974."
-    copyright.innerHTML=aP;
-    copyright.style.border = "none";
-    copyright.style.fontColor = ("green");
-    plusline2.innerHTML=turner;
-    plusline2.style.border = "none";
-    plusline3.innerHTML=leWitt;
-    plusline3.style.border = "none";
-    plusline4.innerHTML=bronze;
-    plusline4.style.border = "none";
-    plusline5.innerHTML=plant;
-    plusline5.style.border = "none";
+        var turner = "J.M.W. Turner, 'The Rooftops of Venice, with the Campanile of San Marco and San Giorgio Maggiore at Sunrise', watercolour on paper, 1840.  Tate Collection."
+        var leWitt = "Sol Lewitt, 'Drawing Series 1968 (Fours)', graphite wall drawing, 1968.";
+        var bronze = "Grand Tour bronze, 19th Century.";
+        var plant = "Areca palm, heavily and heavenly influenced by Marcel Broodthaers, 'Un Jardin d'Hiver', 1974."
+        copyright.innerHTML=aP;
+        copyright.style.border = "none";
+        plusline2.innerHTML=turner;
+        plusline2.style.border = "none";
+        plusline3.innerHTML=leWitt;
+        plusline3.style.border = "none";
+        plusline4.innerHTML=bronze;
+        plusline4.style.border = "none";
+        plusline5.innerHTML=plant;
+        plusline5.style.border = "none";
+        tally = 1
+        }
+    else {
+        var creditTitle = "C R E D I T S";
+        var blank = "";
+        copyright.innerHTML=creditTitle;
+        plusline2.innerHTML=blank; 
+        plusline3.innerHTML=blank; 
+        plusline4.innerHTML=blank; 
+        plusline5.innerHTML=blank; 
+        tally = 0
+    }
+    
 };
