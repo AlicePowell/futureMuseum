22235d7bd57ddafdfcbba3a800b139391df8fb79
Alice Powell <72016384+AlicePowell@users.noreply.github.com>
2021-04-04 12:25:49 +0100

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -6,15 +6,11 @@ function myFunction() {
 myFunction ()
 ;
 
-//document.getElementById("btn").addEventListener("click",
-//    function () {
-//        document.window.location.href = "./mainPage/mainPage.html" });
-
-
 //Refresh page by just clicking
 document.addEventListener("click", function(){
     window.location="./mainPage/mainPage.html"});
 
+    
 //Make a variable x the html of the current page
 var x = location.href;
 console.log(x);
--- mainPage/mainPage.js
+++ mainPage/mainPage.js
@@ -3,10 +3,16 @@
 
 // Start the projection after 16 seconds by adding CSS background image to div
 var light = document.getElementById("projectionContainer")
+var beam = document.getElementById("lightbeam")
+var plant1 = document.getElementById("galleryPlant")
+var plant2 = document.getElementById("plantpotContainer")
 
 function delayProject () {
     setTimeout (function () {
         light.style.backgroundImage = "url('../images/projection.jpeg')";
+        beam.style.opacity = "1";
+        plant1.style.opacity = "0.7";
+        plant2.style.opacity = "0.7";
         }, 13800);}
 delayProject ();
 
@@ -41,7 +47,7 @@ var blank = "";
 
 function credit(id) {
     if (tally == 0) {
-        var aP = "Coding, design and images by Alice Powell, March 2021.";
+        var aP = "&copy; Coding, design and images by Alice Powell, April 2021.";
     // var seurat = "Georges Seurat, 'Reclining Man (study for Bathing Place), conte drawing on paper, 1883. ";
         var turner = "J.M.W. Turner, 'The Rooftops of Venice, with the Campanile of San Marco and San Giorgio Maggiore at Sunrise', watercolour on paper, 1840.  Tate Collection."
         var leWitt = "Sol Lewitt, 'Drawing Series 1968 (Fours)', graphite wall drawing, 1968.";
@@ -50,13 +56,13 @@ function credit(id) {
         var plant = "Areca palm, heavily & heavenly influenced by Marcel Broodthaers, 'Un Jardin d'Hiver', 1974."
         copyright.innerHTML=aP;
         copyright.style.border = "none";
-        plusline2.innerHTML=turner;
+        plusline2.innerHTML=ethanol;
         plusline2.style.border = "none";
-        plusline3.innerHTML=leWitt;
+        plusline3.innerHTML=turner;
         plusline3.style.border = "none";
-        plusline4.innerHTML=bronze;
+        plusline4.innerHTML=leWitt;
         plusline4.style.border = "none";
-        plusline5.innerHTML=ethanol;
+        plusline5.innerHTML=bronze;
         plusline5.style.border = "none";
         plusline6.innerHTML=plant;
         plusline6.style.border = "none";
